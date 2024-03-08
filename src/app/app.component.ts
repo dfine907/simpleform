f6dea6cdf8700a844841bc8026ecd510df64d052
Dina <dinagiacobbi@gmail.com>
2024-03-08 12:03:53 -0900

    Updated entire html layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  email: string = '';
-
-  onSubmit() {
-    console.log(this.email);
-    
-  }
 
 }
